b12dd95721197453851a9e280823e49fd8f29f64
John Varga <john.g.varga@gmail.com>
2017-03-05 10:50:54 -0800

    branch to play around with changing the word

--- steering/Vehicle.js
+++ steering/Vehicle.js
@@ -1,64 +1,68 @@
 function Vehicle(x, y) {
-  this.pos = createVector(random(width), random(height))
-  this.vel = p5.Vector.random2D()
-  this.acc = createVector()
-  this.target = createVector(x, y)
-  this.r = 8
-  this.maxspeed = 10
-  this.maxforce = 1
+    this.pos = createVector(random(width), random(height))
+    this.vel = p5.Vector.random2D()
+    this.acc = createVector()
+    this.target = createVector(x, y)
+    this.r = 4
+    this.maxspeed = 10
+    this.maxforce = 1
+}
+
+Vehicle.prototype.setTarget = function(x, y) {
+    this.target = createVector(x, y)
 }
 
 Vehicle.prototype.behaviors = function() {
-  var arrive = this.arrive(this.target)
-  var mouse = createVector(mouseX, mouseY)
-  var flee = this.flee(mouse)
+    var arrive = this.arrive(this.target)
+    var mouse = createVector(mouseX, mouseY)
+    var flee = this.flee(mouse)
 
-  arrive.mult(1)
-  flee.mult(5)
+    arrive.mult(1)
+    flee.mult(5)
 
-  this.applyForce(arrive)
-  this.applyForce(flee)
+    this.applyForce(arrive)
+    this.applyForce(flee)
 }
 
 Vehicle.prototype.applyForce = function(f) {
-  this.acc.add(f)
+    this.acc.add(f)
 }
 
 Vehicle.prototype.update = function() {
-  this.pos.add(this.vel)
-  this.vel.add(this.acc)
-  this.acc.mult(0)
+    this.pos.add(this.vel)
+    this.vel.add(this.acc)
+    this.acc.mult(0)
 }
 
 Vehicle.prototype.show = function() {
-  stroke(255)
-  strokeWeight(this.r)
-  point(this.pos.x, this.pos.y)
+    stroke(255)
+    strokeWeight(this.r)
+    point(this.pos.x, this.pos.y)
 }
 
 Vehicle.prototype.arrive = function(target) {
-  var desired = p5.Vector.sub(target, this.pos)
-  var dist = desired.mag()
-  var speed = this.maxspeed
-  if (dist < 100) {
-    speed = map(dist, 0, 100, 0, this.maxspeed)
-  }
-  desired.setMag(speed)
-  var steer = p5.Vector.sub(desired, this.vel)
-  steer.limit(this.maxforce)
-  return steer
-}
-
-Vehicle.prototype.flee = function(target) {
-  var desired = p5.Vector.sub(target, this.pos)
-  var d = desired.mag()
-  if (d < 50) {
-    desired.setMag(this.maxspeed)
-    desired.mult(-1)
+    var desired = p5.Vector.sub(target, this.pos)
+    var dist = desired.mag()
+    var speed = this.maxspeed
+    if (dist < 100) {
+        speed = map(dist, 0, 100, 0, this.maxspeed)
+    }
+    desired.setMag(speed)
     var steer = p5.Vector.sub(desired, this.vel)
     steer.limit(this.maxforce)
     return steer
-  } else {
-    return createVector(0, 0)
-  }
+}
+
+Vehicle.prototype.flee = function(target) {
+    var desired = p5.Vector.sub(target, this.pos)
+    var d = desired.mag()
+    if (d < 50) {
+        desired.setMag(this.maxspeed)
+        desired.mult(-1)
+        var steer = p5.Vector.sub(desired, this.vel)
+        steer.limit(this.maxforce)
+        return steer
+    } else {
+        return createVector(0, 0)
+    }
 }
--- steering/steering-behaviors.js
+++ steering/steering-behaviors.js
@@ -1,28 +1,40 @@
 var font;
 var vehicles = []
+var words = []
+var framecount = 0
 
 function preload() {
-  font = loadFont('steering/Yrsa-Bold.otf')
+    font = loadFont('steering/Yrsa-Bold.otf')
 }
 
 function setup() {
-  createCanvas(600, 300)
+    createCanvas(window.innerWidth, window.innerHeight)
 
-  var points = font.textToPoints('spirals', 30, 185, 192)
+    words = [
+        "spirals", "nexus", "horizon"
+    ].map(w => {
+        return font.textToPoints(w, 30, 185, 192)
+    })
+    console.log('-words-', words)
 
-  var totalPoints = points.length;
-  for (var i = 0; i < totalPoints; i++) {
-    var p = points[i];
-    var vehicle = new Vehicle(p.x, p.y)
-    vehicles.push(vehicle)
-  }
+    var points = font.textToPoints('spirals', 30, 185, 192)
+    vehicles = points.map(p => {
+        return new Vehicle(p.x, p.y)
+    })
 }
 
 function draw() {
-  background(51)
-  vehicles.forEach(v => {
-    v.behaviors()
-    v.update()
-    v.show()
-  })
+    background(51)
+    vehicles.forEach(v => {
+        v.behaviors()
+        v.update()
+        v.show()
+    })
+    if (++framecount >= 60) {
+        console.debug(`60 frames went by`)
+        vehicles.forEach(v => {
+            v.setTarget(random(width), random(height))
+        })
+        framecount -= 60
+    }
 }
